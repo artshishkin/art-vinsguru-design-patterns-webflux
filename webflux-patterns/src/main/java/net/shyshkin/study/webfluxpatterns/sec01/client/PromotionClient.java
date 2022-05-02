a1e6d69a46b2710c0d9b550e00c0fb81d3f76bc7
Art <d.art.shishkin@gmail.com>
2022-05-02 18:58:27 +0300

    8. Creating External Service Clients (#1)

---
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec01/client/PromotionClient.java
@@ -0,0 +1,26 @@
+package net.shyshkin.study.webfluxpatterns.sec01.client;
+
+import net.shyshkin.study.webfluxpatterns.sec01.dto.PromotionResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PromotionClient {
+
+    private final WebClient webClient;
+
+    public PromotionClient(
+            WebClient.Builder builder,
+            @Value("${app.external.service.url.promotion}") String serverUrl) {
+        webClient = builder.baseUrl(serverUrl).build();
+    }
+
+    public Mono<PromotionResponse> getPromotion(Integer id) {
+        return webClient.get()
+                .uri("{id}", id)
+                .retrieve()
+                .bodyToMono(PromotionResponse.class);
+    }
+}
