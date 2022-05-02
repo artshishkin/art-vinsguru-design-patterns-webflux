f87ab5f6f6d5e2231fb4e4db7a205f85a8c824c5
Art <d.art.shishkin@gmail.com>
2022-05-02 17:58:46 +0300

    6. Project Setup (#1)

---
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/WebfluxPatternsApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.webfluxpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxPatternsApplication.class, args);
+    }
+
+}
---
+++ webflux-patterns/src/test/java/net/shyshkin/study/webfluxpatterns/WebfluxPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.webfluxpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
