ca7497651f98f165a256e297d1db5209f38a3783
Art <d.art.shishkin@gmail.com>
2022-05-03 11:26:06 +0300

    9. Aggregator Service (#1)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec01/dto/Price.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec01/dto/Price.java
@@ -1,10 +1,16 @@
 package net.shyshkin.study.webfluxpatterns.sec01.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Price {
 
     private Integer listPrice;
---
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec01/service/ProductAggregatorService.java
@@ -0,0 +1,46 @@
+package net.shyshkin.study.webfluxpatterns.sec01.service;
+
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.webfluxpatterns.sec01.client.ProductClient;
+import net.shyshkin.study.webfluxpatterns.sec01.client.PromotionClient;
+import net.shyshkin.study.webfluxpatterns.sec01.client.ReviewClient;
+import net.shyshkin.study.webfluxpatterns.sec01.dto.*;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductAggregatorService {
+
+    private final ProductClient productClient;
+    private final PromotionClient promotionClient;
+    private final ReviewClient reviewClient;
+
+    public Mono<ProductAggregator> aggregate(Integer id) {
+        return Mono.zip(
+                        productClient.getProduct(id),
+                        promotionClient.getPromotion(id),
+                        reviewClient.getReviews(id))
+                .map(t -> toDto(t.getT1(), t.getT2(), t.getT3()));
+    }
+
+    private ProductAggregator toDto(ProductResponse product, PromotionResponse promotion, List<Review> reviews) {
+        var price = Price.builder()
+                .listPrice(product.getPrice())
+                .discount(promotion.getDiscount())
+                .endDate(promotion.getEndDate())
+                .discountedPrice(product.getPrice() * 0.01 * (100 - promotion.getDiscount()))
+                .amountSaved(product.getPrice() * promotion.getDiscount() / 100)
+                .build();
+        return ProductAggregator.builder()
+                .id(product.getId())
+                .category(product.getCategory())
+                .description(product.getDescription())
+                .price(price)
+                .reviews(reviews)
+                .build();
+    }
+
+}
