279dcaf853bce5767a72f41883113f568b2115cc
Art <d.art.shishkin@gmail.com>
2022-05-06 12:26:54 +0300

    31.1 Creating Service Clients - ProductClient (#3)

---
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec03/client/ProductClient.java
@@ -0,0 +1,27 @@
+package net.shyshkin.study.webfluxpatterns.sec03.client;
+
+import net.shyshkin.study.webfluxpatterns.sec03.dto.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductClient {
+
+    private final WebClient webClient;
+
+    public ProductClient(
+            WebClient.Builder builder,
+            @Value("${app.external.service.url.sec03.product}") String serverUrl) {
+        webClient = builder.baseUrl(serverUrl).build();
+    }
+
+    public Mono<Product> getProduct(Integer id) {
+        return webClient.get()
+                .uri("/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .onErrorResume(ex -> Mono.empty());
+    }
+}
