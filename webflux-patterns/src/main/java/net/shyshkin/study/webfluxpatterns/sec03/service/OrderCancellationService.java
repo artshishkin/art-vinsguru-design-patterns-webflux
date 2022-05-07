3888ba7fc16ec399a1a6d2c2538510827b8a0418
Art <d.art.shishkin@gmail.com>
2022-05-07 19:59:24 +0300

    39. Order Cancellation Service (#3)

---
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec03/service/OrderCancellationService.java
@@ -0,0 +1,34 @@
+package net.shyshkin.study.webfluxpatterns.sec03.service;
+
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.webfluxpatterns.sec03.dto.OrchestrationRequestContext;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.core.scheduler.Schedulers;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderCancellationService {
+
+    private final List<Orchestrator> orchestrators;
+
+    private Sinks.Many<OrchestrationRequestContext> sink;
+    private Flux<OrchestrationRequestContext> flux;
+
+    @PostConstruct
+    private void init() {
+        sink = Sinks.many().multicast().onBackpressureBuffer();
+        flux = sink.asFlux().publishOn(Schedulers.boundedElastic());
+        orchestrators.forEach(orc -> this.flux.subscribe(orc.cancel()));
+    }
+
+    public void cancelOrder(OrchestrationRequestContext ctx) {
+        sink.tryEmitNext(ctx);
+    }
+
+
+}
