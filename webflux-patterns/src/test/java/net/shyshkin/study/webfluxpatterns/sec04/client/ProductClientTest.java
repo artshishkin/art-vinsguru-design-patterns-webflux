4c4f561638ef6e059aa194cd8ab16cb5db721c43
Art <d.art.shishkin@gmail.com>
2022-05-08 17:43:03 +0300

    51.2 Product Client Test (#4)

---
+++ webflux-patterns/src/test/java/net/shyshkin/study/webfluxpatterns/sec04/client/ProductClientTest.java
@@ -0,0 +1,47 @@
+package net.shyshkin.study.webfluxpatterns.sec04.client;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.webfluxpatterns.common.ExternalServiceAbstractTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@ActiveProfiles("sec04")
+class ProductClientTest extends ExternalServiceAbstractTest {
+
+    @Autowired
+    ProductClient client;
+
+    @Test
+    void getProduct_ok() {
+        //given
+        Integer productId = 1;
+
+        //when
+        StepVerifier.create(client.getProduct(productId))
+
+                //then
+                .consumeNextWith(product -> assertAll(
+                        () -> assertThat(product).hasNoNullFieldsOrProperties(),
+                        () -> assertThat(product.getId()).isEqualTo(productId)
+                ))
+                .verifyComplete();
+    }
+
+    @Test
+    void getProduct_absent() {
+        //given
+        Integer productId = 51;
+
+        //when
+        StepVerifier.create(client.getProduct(productId))
+
+                //then
+                .verifyComplete();
+    }
+}
