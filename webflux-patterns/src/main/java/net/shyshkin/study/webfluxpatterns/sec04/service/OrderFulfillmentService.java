73472608d9f3fc629dc2be7f210815f81fbbec9f
Art <d.art.shishkin@gmail.com>
2022-05-09 13:37:09 +0300

    55.1 Sequential Workflow - Implementation - OrderFulfillmentService (#4)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec04/service/OrderFulfillmentService.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec04/service/OrderFulfillmentService.java
@@ -2,37 +2,45 @@ package net.shyshkin.study.webfluxpatterns.sec04.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.webfluxpatterns.sec04.client.ProductClient;
 import net.shyshkin.study.webfluxpatterns.sec04.dto.OrchestrationRequestContext;
+import net.shyshkin.study.webfluxpatterns.sec04.dto.Product;
 import net.shyshkin.study.webfluxpatterns.sec04.dto.Status;
+import net.shyshkin.study.webfluxpatterns.sec04.util.OrchestrationUtil;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderFulfillmentService {
 
-    private final List<Orchestrator> orchestrators;
+    private final ProductClient productClient;
+    private final PaymentOrchestrator paymentOrchestrator;
+    private final InventoryOrchestrator inventoryOrchestrator;
+    private final ShippingOrchestrator shippingOrchestrator;
 
     public Mono<OrchestrationRequestContext> placeOrder(OrchestrationRequestContext ctx) {
 
-        var list = orchestrators
-                .stream()
-                .map(orchestrator -> orchestrator.create(ctx))
-                .collect(Collectors.toList());
-
-        return Mono.zip(list, a -> a[0])
-                .cast(OrchestrationRequestContext.class)
-                .doOnNext(this::updateStatus);
+        return this.getProduct(ctx)
+                .doOnNext(OrchestrationUtil::buildPaymentRequest)
+                .flatMap(paymentOrchestrator::create)
+                .doOnNext(OrchestrationUtil::buildInventoryRequest)
+                .flatMap(inventoryOrchestrator::create)
+                .doOnNext(OrchestrationUtil::buildShippingRequest)
+                .flatMap(shippingOrchestrator::create)
+                .doOnNext(ctx1 -> ctx1.setStatus(Status.SUCCESS))
+                .doOnError(ex -> ctx.setStatus(Status.FAILED))
+                .onErrorReturn(ctx);
     }
 
-    private void updateStatus(OrchestrationRequestContext ctx) {
-        boolean allSuccess = this.orchestrators.stream().allMatch(o -> o.isSuccess().test(ctx));
-        var status = allSuccess ? Status.SUCCESS : Status.FAILED;
-        ctx.setStatus(status);
+    private Mono<OrchestrationRequestContext> getProduct(OrchestrationRequestContext ctx) {
+        Integer productId = ctx.getOrderRequest().getProductId();
+        return productClient
+                .getProduct(productId)
+                .map(Product::getPrice)
+                .doOnNext(ctx::setProductPrice)
+                .map(i -> ctx);
     }
 
 }
