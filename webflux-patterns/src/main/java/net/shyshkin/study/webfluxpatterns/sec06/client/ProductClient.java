e52c9399a244aa64b72f443fa886e597e92d1744
Art <d.art.shishkin@gmail.com>
2022-05-11 14:26:06 +0300

    74. Timeout Pattern Implementation (#9)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec06/client/ProductClient.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec06/client/ProductClient.java
@@ -1,11 +1,15 @@
 package net.shyshkin.study.webfluxpatterns.sec06.client;
 
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.webfluxpatterns.sec06.dto.Product;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
+@Slf4j
 @Component
 public class ProductClient {
 
@@ -22,6 +26,8 @@ public class ProductClient {
                 .uri("/{id}", id)
                 .retrieve()
                 .bodyToMono(Product.class)
+                .timeout(Duration.ofMillis(500))
+                .doOnError(ex -> log.debug("Ex: {}", ex.toString()))
                 .onErrorResume(ex -> Mono.empty());
     }
 }
--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec06/client/ReviewClient.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec06/client/ReviewClient.java
@@ -1,13 +1,16 @@
 package net.shyshkin.study.webfluxpatterns.sec06.client;
 
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.webfluxpatterns.sec06.dto.Review;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 
+@Slf4j
 @Component
 public class ReviewClient {
 
@@ -25,6 +28,8 @@ public class ReviewClient {
                 .retrieve()
                 .bodyToFlux(Review.class)
                 .collectList()
+                .timeout(Duration.ofMillis(500))
+                .doOnError(ex -> log.debug("Ex: {}", ex.toString()))
                 .onErrorReturn(List.of());
     }
 }
