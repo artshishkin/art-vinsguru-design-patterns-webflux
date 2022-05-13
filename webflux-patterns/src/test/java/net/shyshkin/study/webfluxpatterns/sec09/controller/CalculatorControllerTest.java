bf9109edbec293e864892e046c0e14d4b40b4ad6
Art <d.art.shishkin@gmail.com>
2022-05-13 18:22:55 +0300

    96. Server Side Rate Limiter - Demo (#12)

--- webflux-patterns/src/test/java/net/shyshkin/study/webfluxpatterns/sec09/controller/CalculatorControllerTest.java
+++ webflux-patterns/src/test/java/net/shyshkin/study/webfluxpatterns/sec09/controller/CalculatorControllerTest.java
@@ -2,15 +2,17 @@ package net.shyshkin.study.webfluxpatterns.sec09.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.webfluxpatterns.common.ExternalServiceAbstractTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("sec09")
 class CalculatorControllerTest extends ExternalServiceAbstractTest {
@@ -18,10 +20,11 @@ class CalculatorControllerTest extends ExternalServiceAbstractTest {
     @Autowired
     WebTestClient webClient;
 
-    @Test
-    void doubleInput() {
+    @RepeatedTest(5)
+    @Order(10)
+    void doubleInput_ok(RepetitionInfo repetitionInfo) {
         //given
-        int input = 4;
+        int input = repetitionInfo.getCurrentRepetition();
 
         //when
         webClient.get()
@@ -33,4 +36,23 @@ class CalculatorControllerTest extends ExternalServiceAbstractTest {
                 .expectBody(Integer.TYPE)
                 .value(res -> assertThat(res).isEqualTo(input * 2));
     }
+
+    @RepeatedTest(5)
+    @Order(20)
+    void doubleInput_rateLimit(RepetitionInfo repetitionInfo) {
+        //given
+        int input = repetitionInfo.getCurrentRepetition();
+        String expectedErrorMessage = "RateLimiter 'calculator-service' does not permit further calls";
+
+        //when
+        webClient.get()
+                .uri("/sec09/calculator/{input}", input)
+                .exchange()
+
+                //then
+                .expectStatus().isEqualTo(HttpStatus.TOO_MANY_REQUESTS)
+                .expectBody(String.class)
+                .value(res -> assertThat(res).isEqualTo(expectedErrorMessage));
+    }
+
 }
