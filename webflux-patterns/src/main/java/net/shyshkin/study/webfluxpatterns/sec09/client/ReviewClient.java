b3c4b47a44aaeeae7671bb2d7e85a0e49c287c42
Art <d.art.shishkin@gmail.com>
2022-05-13 21:37:00 +0300

    97.1 Client Side Rate Limiter - Implementation (#12)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec09/client/ReviewClient.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec09/client/ReviewClient.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.webfluxpatterns.sec09.client;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.webfluxpatterns.sec09.dto.Review;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
 
-import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -24,17 +23,18 @@ public class ReviewClient {
         webClient = builder.baseUrl(serverUrl).build();
     }
 
+    @RateLimiter(name = "review-service", fallbackMethod = "fallback")
     public Mono<List<Review>> getReviews(Integer id) {
         return webClient.get()
                 .uri("/{id}", id)
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, response -> Mono.empty())
                 .bodyToFlux(Review.class)
-                .collectList()
-                .doOnError(ex -> log.debug("Ex: {}", ex.toString()))
-//                .retry(5)
-                .retryWhen(Retry.fixedDelay(6, Duration.ofMillis(50)))
-                .timeout(Duration.ofMillis(700))
-                .onErrorReturn(List.of());
+                .collectList();
+    }
+
+    public Mono<List<Review>> fallback(Integer id, Throwable ex) {
+        log.debug("Rate Limit for Review Service with exception: {}", ex.toString());
+        return Mono.just(List.of());
     }
 }
