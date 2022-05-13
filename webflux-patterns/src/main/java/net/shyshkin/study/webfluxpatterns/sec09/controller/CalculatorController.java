2c02f75cfb1a21fdc7043a4f33b26730fcde7f6b
Art <d.art.shishkin@gmail.com>
2022-05-13 18:10:49 +0300

    95.3 Server Side Rate Limiter - Implementation (#12)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec09/controller/CalculatorController.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec09/controller/CalculatorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("sec09/calculator")
 public class CalculatorController {
@@ -17,9 +18,16 @@ public class CalculatorController {
     //CPU Intensive Task
     //We want to allow only 5 request per 20 seconds
     @GetMapping("{input}")
+    @RateLimiter(name = "calculator-service", fallbackMethod = "fallback")
     public Mono<ResponseEntity<Integer>> doubleInput(@PathVariable Integer input) {
         return Mono.fromSupplier(() -> input * 2)
                 .map(ResponseEntity::ok);
     }
 
+    public Mono<ResponseEntity<String>> fallback(Integer input, Throwable ex) {
+        log.debug("Rate Limiter Fallback method with error: {}", ex.toString());
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.TOO_MANY_REQUESTS)
+                .body(ex.getMessage()));
+    }
 }
