6d4834bf48cdd13d7b32ffdd48552762fe899dc8
Art <d.art.shishkin@gmail.com>
2022-05-14 16:53:05 +0300

    103. Bulkhead Pattern Implementation (#13)

--- webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec10/controller/FibController.java
+++ webflux-patterns/src/main/java/net/shyshkin/study/webfluxpatterns/sec10/controller/FibController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @RestController
 @RequestMapping("sec10")
 public class FibController {
 
+    //I have 8 CPUs but I like to limit 6 CPUs just for fibonacci operation
+    private final Scheduler scheduler = Schedulers.newParallel("fib", 6);
+
     //CPU Intensive Task
     @GetMapping("fib/{input}")
     public Mono<ResponseEntity<Long>> fibonacci(@PathVariable Long input) {
         return Mono.fromSupplier(() -> findFib(input))
+                .subscribeOn(scheduler)
                 .map(ResponseEntity::ok);
     }
 
